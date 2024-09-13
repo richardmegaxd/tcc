bc90527b690128c4f8d23f82b86ab7de857246a9
Andre-LuizP <andreluizposilva@gmail.com>
2024-09-13 11:12:56 -0300

    Update home.php

--- TCC-main/arquivos-php/home.php
+++ TCC-main/arquivos-php/home.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOME</title>
-    <link rel="stylesheet" href="../home-assets/css/style8.css">
+    <link rel="stylesheet" href="../home-assets/css/style.css">
     <link rel="stylesheet" href="../home-assets/css/lightslider.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
