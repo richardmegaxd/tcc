7ff99641d9dd00e88b6608b00d0a0297710ac826
marceloaugustojr <junior-fde@hotmail.com>
2024-09-13 22:21:19 -0300

    Update login.php

--- TCC-main/arquivos-php/login.php
+++ TCC-main/arquivos-php/login.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login The Glarck</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/css/styleLogin3.css">
+    <link rel="stylesheet" href="../assets/css/styleLogin.css">
 </head>
 
 <body>
