7bf73e9cb941fdd76d99a0639e02c588039cda36
Marcelo Azevedo <151553837+marceloaugustojr@users.noreply.github.com>
2024-09-30 13:59:26 -0300

    Update home.php

--- TCC-main/arquivos-php/home.php
+++ TCC-main/arquivos-php/home.php
@@ -452,6 +452,168 @@
     </main>
     <!-- # FIM LEITURA DO MANGÁ -->
 
+
+
+
+
+
+
+
+
+
+
+
+    <footer class="footer" id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+
+        <div class="footer-manga">
+
+          <a href="#" class="logo">The GLARK</a>
+
+          <p class="footer-text">
+            Dê vida às suas histórias favoritas de forma legal e segura.
+          </p>
+
+          <ul class="contact-list">
+
+            <li class="contact-item">
+              <div class="contact-icon">
+                <ion-icon name="location"></ion-icon>
+              </div>
+
+              <address class="item-text">
+                Endereço : São Vicente - SP
+              </address>
+            </li>
+
+            <li class="contact-item">
+              <div class="contact-icon">
+                <ion-icon name="headset"></ion-icon>
+              </div>
+
+              <a href="tel:+241245654235" class="item-text">Telefone : +55 (13)94565-4235</a>
+            </li>
+
+            <li class="contact-item">
+              <div class="contact-icon">
+                <ion-icon name="mail-open"></ion-icon>
+              </div>
+
+              <a href="mailto:info@exemple.com" class="item-text">Email : theglark@gmail.com </a>
+            </li>
+
+          </ul>
+
+        </div>
+
+        <ul class="footer-list">
+
+          <li>
+            <p class="footer-list-title">Navegação</p>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link">Obras Populares</a>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link">Lançamentos</a>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link">Notícias</a>
+          </li>
+
+
+        </ul>
+
+        <ul class="footer-list">
+
+          <li>
+            <p class="footer-list-title">PRECISA DE AJUDA?</p>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link">Termos e Condições</a>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link">Política de Privacidade</a>
+          </li>
+
+          <li>
+            <a href="#" class="footer-link">Política de reembolso</a>
+          </li>
+
+        </ul>
+
+        <div class="footer-wrapper">
+
+          <div class="social-wrapper">
+
+            <p class="footer-list-title">SIGA-NOS</p>
+
+            <ul class="social-list">
+
+              <li>
+                <a href="#" class="social-link" style="background-color: #3b5998">
+                  <ion-icon name="logo-facebook"></ion-icon>
+                </a>
+              </li>
+
+              <li>
+                <a href="#" class="social-link"
+                  style="background-image: radial-gradient(circle at -20.71% 50%, #afde72 0, #bbd966 5.56%, #c6d259 11.11%, #cfc94e 16.67%, #d8c042 22.22%, #dfb537 27.78%, #e6a92e 33.33%, #eb9c26 38.89%, #f08c21 44.44%, #f37b1f 50%, #f56822 55.56%, #f85329 61.11%, #f93c32 66.67%, #fa193d 72.22%, #f90049 77.78%, #f80057 83.33%, #f40066 88.89%, #ef0076 94.44%, #e70087 100%);">
+                  <ion-icon name="logo-instagram"></ion-icon>
+                </a>
+              </li>
+
+              <li>
+                <a href="#" class="social-link" style="background-color: #1565c0">
+                  <ion-icon name="logo-linkedin"></ion-icon>
+                </a>
+              </li>
+
+            </ul>
+
+          </div>
+
+          <div class="footer-duvida">
+
+            <p class="footer-list-title">Quer saber quando o próximo capítulo vai ser lançado?</p>
+
+            <form action="" class="footer-duvida">
+              <input type="email" name="email_address" aria-label="email" placeholder="Digite seu e-mail" required
+                class="email-field">
+
+              <button type="submit" class="footer-btn" aria-label="submit">
+                <ion-icon name="rocket"></ion-icon>
+              </button>
+            </form>
+
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
+
+    <!-- 
+    - #BACK TO TOP
+  -->
+
+    <a href="#top" class="back-top-btn" aria-label="back to top" data-back-top-btn>
+      <ion-icon name="caret-up"></ion-icon>
+    </a>
+
+
+
+
+
+    
     <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
     <script src="../home-assets/Js/JQuery3.3.1.js"></script>
     <script src="../home-assets/Js/lightslider.js"></script>
