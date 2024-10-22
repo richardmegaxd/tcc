a1e9961456d8ba1950e741fccb842f16ef6d3d97
Andre-LuizP <149334735+Andre-LuizP@users.noreply.github.com>
2024-10-22 15:15:49 -0300

    Update edit.php

--- TCC/arquivos-php/edit.php
+++ TCC/arquivos-php/edit.php
@@ -40,10 +40,10 @@
     ?>
 
     <body>
-
+        
         <div class="login-container">
             <h1>Editar Perfil</h1>
-            <form action="validaEdit.php" method="POST">
+            <form id="form" action="validaEdit.php" method="POST">
                 <div class="input-container">
                     <input type="text" id="nome" name="nome" value="<?php echo htmlspecialchars($user['nm_user']); ?>"><br>
                     <label for="nome">Nome:</label>
@@ -51,12 +51,24 @@
 
                 <div class="input-container">
                     <input type="text" id="apelido" name="apelido" value="<?php echo htmlspecialchars($user['nm_apelido']); ?>" ><br>
-                    <label for="idade">Apelido:</label>
+                    <label for="apelido">Apelido:</label>
+                </div>
+
+                <div class="input-container">
+                    <input type="password" id="new-password" name="senha" minlength="6" value="<?php echo htmlspecialchars($user['ds_senha']); ?>" ><br>
+                    <label for="senha">Nova Senha:</label>
+                    <p class="text-senha">A senha deve conter no mínimo 6 caracteres</p>
+                </div>
+
+                <div class="input-container">
+                    <input type="password" id="confirm-new-password" minlength="6" value="<?php echo htmlspecialchars($user['ds_senha']); ?>" ><br>
+                    <label for="senha">Confirme a Senha:</label>
+                    <p id="pass-error" class="error-message" style="display:none;">As senhas não coincidem</p>
                 </div>
 
                 <div class="input-container">
                     <input type="text" id="email" name="email" value="<?php echo htmlspecialchars($user['ds_email']); ?>" ><br>
-                    <label for="email">email:</label>
+                    <label for="email">Email:</label>
                 </div>
 
 
@@ -67,6 +79,32 @@
         </div>
         <a href="home.php">Cancelar</a>
 
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+        <script>
+        const password = document.getElementById('new-password');
+        const confirmPassword = document.getElementById('confirm-new-password');
+        const errorMessage = document.getElementById('pass-error');
+
+        const form = document.getElementById('form');
+
+        confirmPassword.addEventListener('input', function() {
+            if (confirmPassword.value !== password.value) {
+                errorMessage.style.display = 'block';
+                confirmPassword.style.borderColor = 'red';
+            } else {
+                errorMessage.style.display = 'none';
+                confirmPassword.style.borderColor = 'rgb(87, 0, 128)';
+            }
+        });
+
+        form.addEventListener('submit', function(event) {
+            if (confirmPassword.value !== password.value) {
+                event.preventDefault();
+                alert('Confira novamente os dados cadastrados.');
+            }
+        });
+        </script>
     </body>
 
     </html>
