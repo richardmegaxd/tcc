1d7492c96bfcb2ec0737bd3beb2d521449d47c9e
Andre-LuizP <149334735+Andre-LuizP@users.noreply.github.com>
2024-10-24 14:58:41 -0300

    Update valida_google_login.php

--- tcc/arquivos-php/valida_google_login.php
+++ tcc/arquivos-php/valida_google_login.php
@@ -11,6 +11,10 @@ if (mysqli_connect_errno()) {
 // Captura o email e a URL da foto da requisição
 $email = $_POST['email'];
 $photoUrl = $_POST['photo_url']; 
+$nome = $_POST['nome'];
+$sobrenome = $_POST['sobrenome'];
+
+$nome_completo = $nome . ' ' . $sobrenome;
 
 // Verifica se o usuário já existe
 $seleciona_usuario = "SELECT * FROM tb_usuario WHERE ds_email = '$email'";
@@ -19,11 +23,11 @@ $checar_usuario = mysqli_num_rows($procura);
 
 if ($checar_usuario > 0) {
     // O usuário já existe, apenas atualize a foto
-    $queryUpdate = "UPDATE tb_usuario SET ds_foto_perfil = '$photoUrl' WHERE ds_email = '$email'";
+    $queryUpdate = "UPDATE tb_usuario SET ds_foto_perfil = '$photoUrl', nm_user = '$nome_completo', login_google = 1 WHERE ds_email = '$email'";
     mysqli_query($conexao, $queryUpdate);
 } else {
     // O usuário não existe, insira um novo registro
-    $queryInsert = "INSERT INTO tb_usuario (ds_email, ds_foto_perfil) VALUES ('$email', '$photoUrl')";
+    $queryInsert = "INSERT INTO tb_usuario (ds_email, ds_foto_perfil, nm_user, login_google) VALUES ('$email', '$photoUrl', '$nome_completo', 1)";
     mysqli_query($conexao, $queryInsert);
 
     $_SESSION['image'] = $photoUrl;
