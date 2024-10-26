df28d8617c981f573d6d3228e39f70d23d99e75d
Andre-LuizP <andreluizposilva@gmail.com>
2024-10-25 22:52:48 -0300

    Update validarlogin.php

--- tcc/arquivos-php/validarlogin.php
+++ tcc/arquivos-php/validarlogin.php
@@ -1,43 +1,55 @@
 <?php
 session_start();
 
-$_SESSION['logado'] = false; // variável global de sessão logado
-
 $conexao = mysqli_connect("localhost", "root", "", "bd_glark", "3306");
 
-if (mysqli_connect_errno()) // verifica se ocorreu um erro na conexão com o banco de dados
-{
-    echo "A conexão MYSQL apresentou erro: " . mysqli_connect_error(); // descreve o erro que ocorreu
+if (mysqli_connect_errno()) {
+    echo "A conexão MYSQL apresentou erro: " . mysqli_connect_error();
+    exit;
 }
 
 if (isset($_POST["login"]) && isset($_POST["senha"])) {
-    $loginUsuario = $_POST["login"]; // converte o que inserido no post em uma variável comum
+    $loginUsuario = $_POST["login"];
     $senhaUsuario = $_POST["senha"];
 
     // Sanitizar os dados
     $loginUsuario = mysqli_real_escape_string($conexao, $loginUsuario);
     $senhaUsuario = mysqli_real_escape_string($conexao, $senhaUsuario);
 
-    $seleciona_usuario = "SELECT * FROM tb_usuario WHERE ds_email = '$loginUsuario' AND ds_senha = '$senhaUsuario'"; // efetua a seleção no banco de dados e atribui a uma variável
+    // Verifica se a conta está ativa
+    $queryAtivo = "SELECT * FROM tb_usuario WHERE ds_email = '$loginUsuario' AND ds_senha = '$senhaUsuario' AND conta_ativa = 1";
+    $procuraAtivo = mysqli_query($conexao, $queryAtivo);
 
-    $procura = mysqli_query($conexao, $seleciona_usuario); // Realiza uma consulta no banco de dados
+    if (mysqli_num_rows($procuraAtivo) > 0) {
+        $usuario = mysqli_fetch_assoc($procuraAtivo);
+        $idUsuario = $usuario['cd_usuario'];
+        $nomeUsuario = $usuario['nm_user'];
+        $apelidoUsuario = $usuario['nm_apelido']; // Armazena o nome completo do usuário
 
-    $checar_usuario = mysqli_num_rows($procura); // verifica quantas linhas correspondente a busca feita no banco de dados
-
-    if ($checar_usuario > 0) {
-        // Se o usuário existe, recupere o ID do usuário
-        $usuario = mysqli_fetch_assoc($procura);
-        $idUsuario = $usuario['id_usuario'];
-
-        // Defina o ID do usuário na sessão
+        // Define as variáveis de sessão
         $_SESSION['logado'] = true;
         $_SESSION['usuario'] = $loginUsuario;
-        $_SESSION['id_usuario'] = $idUsuario;
+        $_SESSION['cd_usuario'] = $idUsuario;
+        $_SESSION['nome'] = $nomeUsuario;
+        $_SESSION['apelido'] = $apelidoUsuario; // Define o nome na sessão
 
         header("Location: home.php");
     } else {
-        $erro = "Login ou senha incorretos. Por favor, tente novamente.";
-        header("Location: login.php?erro=" . urlencode($erro));
+        // Verifica se a conta existe, mas está inativa
+        $queryInativo = "SELECT * FROM tb_usuario WHERE ds_email = '$loginUsuario' AND ds_senha = '$senhaUsuario' AND conta_ativa = 0";
+        $procuraInativo = mysqli_query($conexao, $queryInativo);
+
+        if (mysqli_num_rows($procuraInativo) > 0) {
+            // Salva o ID e o nome do usuário para confirmar reativação
+            $usuario = mysqli_fetch_assoc($procuraInativo);
+            $_SESSION['temp_usuario'] = $usuario['cd_usuario'];
+            $_SESSION['nome'] = $usuario['nm_user']; // Define o nome temporariamente para exibir na confirmação
+
+            header("Location: conf_ativacao.php");
+        } else {
+            $erro = "Login ou senha incorretos. Por favor, tente novamente.";
+            header("Location: login.php?erro=" . urlencode($erro));
+        }
     }
 } else {
     $erro = "Por favor, preencha todos os campos.";
@@ -45,3 +57,4 @@ if (isset($_POST["login"]) && isset($_POST["senha"])) {
 }
 
 mysqli_close($conexao);
+?>
