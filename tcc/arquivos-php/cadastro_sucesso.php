846c98644397567c16f51c8e7b5dd28a9e35ada3
Andre-LuizP <andreluizposilva@gmail.com>
2024-10-25 22:52:20 -0300

    Create cadastro_sucesso.php

---
+++ tcc/arquivos-php/cadastro_sucesso.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro The Glarck</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/styleLogin.css">
+</head>
+
+<body>
+
+
+    <header>
+        <div class="logo">
+            <img src="../assets/images/log5.png" width="50%" height="auto" loading="lazy" alt="Logotipo The Glarck" class="card-icon">
+        </div>
+        <h1>Conta criada com sucesso!</h1>
+    </header>
+
+    <a href="login.php" class="voltar">Retornar</a>
+
+</body>
+
+</html>
