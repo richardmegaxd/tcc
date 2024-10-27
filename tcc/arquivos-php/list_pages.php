7a36f2987ca25f4e7a16a73723836d1fe7f6f33e
Marcelo Azevedo <151553837+marceloaugustojr@users.noreply.github.com>
2024-10-27 17:07:38 -0300

    Update list_pages.php

--- tcc/arquivos-php/list_pages.php
+++ tcc/arquivos-php/list_pages.php
@@ -1,11 +1,11 @@
 <?php
 // Diretório onde as páginas do gibi estão armazenadas
-$directory = '../capitulo1/'; // Mude para o diretório específico
+$directory = '../capitulo2/'; // Mude para o diretório específico
 
 // Verifica se o diretório existe
 if (is_dir($directory)) {
     // Obtém todas as imagens com extensão .jpg, .jpeg, .png ou .gif
-    $images = glob($directory . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
+    $images = glob($directory . "*.{jpg,jpeg,png,gif,pdf}", GLOB_BRACE);
 
     // Retorna a lista de arquivos no formato JSON
     echo json_encode($images);
