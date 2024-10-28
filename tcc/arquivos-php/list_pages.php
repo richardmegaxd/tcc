ab83c5d58bf217fe628a5c8c20d577cea825ac64
Marcelo Azevedo <151553837+marceloaugustojr@users.noreply.github.com>
2024-10-27 22:33:58 -0300

    Update list_pages.php

--- tcc/arquivos-php/list_pages.php
+++ tcc/arquivos-php/list_pages.php
@@ -5,7 +5,7 @@ $directory = '../capitulo2/'; // Mude para o diretório específico
 // Verifica se o diretório existe
 if (is_dir($directory)) {
     // Obtém todas as imagens com extensão .jpg, .jpeg, .png ou .gif
-    $images = glob($directory . "*.{jpg,jpeg,png,gif,pdf}", GLOB_BRACE);
+    $images = glob($directory . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
 
     // Retorna a lista de arquivos no formato JSON
     echo json_encode($images);
