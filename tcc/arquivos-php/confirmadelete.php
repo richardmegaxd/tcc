af0117872cc45768c2e81aa6305e32eeefeef063
Andre-LuizP <149334735+Andre-LuizP@users.noreply.github.com>
2024-10-28 14:53:55 -0300

    Update confirmadelete.php

--- tcc/arquivos-php/confirmadelete.php
+++ tcc/arquivos-php/confirmadelete.php
@@ -6,15 +6,28 @@
     }
     else{
      ?> 
-    <div class="prin">
-    <div class="form">  
-     <h1>Deseja Deletar seu usuário?</h1>
+     <!DOCTYPE html>
+     <html lang="en">
+     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="../assets/css/styleLogin.css">
+     </head>
+     <body>
+        
+     </body>
+     </html>
+     <div id="section-delete" class="logo">
+     <img src="../assets/images/log5.png" width="50%" height="auto" loading="lazy" alt="Logotipo The Glarck" class="card-icon">
+     <h1>The Glark</h1><br>
+     </div> 
+     <h2>Deseja Deletar seu usuário?</h2>
      <br>
-        <a href="delete.php" class="a1"> Sim </a>
+        <a href="delete.php" class="voltar delete" > <b>Sim</b> </a>
      <br>
-        <a href="home.php"  class="a1">Não! Quero voltar ao Menu!</a>
-    </div>
-    </div>
+        <a href="home.php" class="voltar delete"> <b>Não! Voltar ao Menu!</b> </a>  
     <?php
     }
     ?>
