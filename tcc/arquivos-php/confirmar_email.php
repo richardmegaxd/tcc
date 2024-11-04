f76760fe2d8f9f1e344c27f743325f6621c00765
Andre-LuizP <149334735+Andre-LuizP@users.noreply.github.com>
2024-11-04 14:53:22 -0300

    Create confirmar_email.php

---
+++ tcc/arquivos-php/confirmar_email.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Confirme seu Email</title>
+</head>
+<body>
+    <h1>Confirmação de Email</h1>
+    <form action="validar_codigo.php" method="POST">
+        <label for="codigo">Insira o código de confirmação:</label>
+        <input type="text" name="codigo" required>
+        <input type="email" name="email" placeholder="Email cadastrado" required>
+        <button type="submit">Confirmar</button>
+    </form>
+</body>
+</html>
