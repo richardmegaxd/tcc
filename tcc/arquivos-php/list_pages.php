61d8c2a04537df8a7a8e0d59ad881cd276d8ab29
Andre-LuizP <149334735+Andre-LuizP@users.noreply.github.com>
2024-11-21 14:54:13 -0300

    Update list_pages.php

--- tcc/arquivos-php/list_pages.php
+++ tcc/arquivos-php/list_pages.php
@@ -1,15 +1,15 @@
 <?php
-// Diretório onde as páginas do gibi estão armazenadas
-$directory = '../capitulo2/'; // Mude para o diretório específico
+// Diretório base onde as obras estão armazenadas
+$directory = '../obras/mangas';
 
 // Verifica se o diretório existe
 if (is_dir($directory)) {
     // Obtém todas as imagens com extensão .jpg, .jpeg, .png ou .gif
-    $images = glob($directory . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
+    $images = glob($directory . "/*.{jpg,jpeg,png,gif}", GLOB_BRACE);
 
-    // Retorna a lista de arquivos no formato JSON
+    // Retorna os caminhos das imagens no formato JSON
     echo json_encode($images);
 } else {
-    echo json_encode([]);
+    echo json_encode([]); // Retorna um array vazio se o diretório não existir
 }
 ?>
