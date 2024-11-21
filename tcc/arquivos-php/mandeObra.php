8bcdb120e4c9cf6ba9ae92b44b5110a29840e4d0
Andre-LuizP <149334735+Andre-LuizP@users.noreply.github.com>
2024-11-21 14:54:17 -0300

    Update mandeObra.php

---
+++ tcc/arquivos-php/mandeObra.php
@@ -0,0 +1,15 @@
+<form action="publicar_obra.php" method="POST" enctype="multipart/form-data">
+    <label for="titulo">Título do Manga</label>
+    <input type="text" id="titulo" name="titulo" required>
+
+    <label for="descricao">Descrição</label>
+    <textarea id="descricao" name="descricao" required></textarea>
+
+    <label for="imagem_capa">Imagem de Capa</label>
+    <input type="file" id="imagem_capa" name="imagem_capa">
+
+    <label for="paginas_manga">Páginas do Manga (Selecione várias)</label>
+    <input type="file" id="paginas_manga[]" name="paginas_manga[]" multiple>
+
+    <input type="submit" value="Publicar Manga">
+</form>
